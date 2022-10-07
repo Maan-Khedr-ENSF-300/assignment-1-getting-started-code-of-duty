bdccaafa3c6916e630e150a88877e6033c9314eb
IshaHaider <isha.haider213@gmail.com>
2022-10-07 15:21:12 -0600

    add function names

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 
-print("Hello World!")
-print('Hi')
+def myAdd():
+    return
+
+def mySub():
+    return
+
+def myMul():
+    return
+
+def myDiv():
+    return
+
+def validate_integer():
+    return
+
+def validate_operator():
+    return
+
+def evaluate_expression():
+    return
+
+def display():
+    return
+
+def main():
+    return
+
+def __name___():
+    main()
