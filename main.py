784e36f545ef8e9fa231c916fb014b0cc7d63ea1
Saba <sabayara0@gmail.com>
2022-10-09 01:00:58 -0600

    def evaluation_expression

--- main.py
+++ main.py
@@ -36,10 +36,45 @@ def validate_operator(operator, str):
         operator = input(('Please enter the {} operator: ').format(str))
     return 1
 
-def evaluate_expression(operator_list, integers_list):  
+def evaluate_expression(operator_list, integers_list): 
+    value_list = []
+    value_list = [int(i) for i in integers_list]
+
+    if (operator_list[0] == '/') and (operator_list[1] == '/'):
+        final_result = myDiv((myDiv(value_list[0], value_list[1])), value_list[2])
+    if (operator_list[0] == '/') and (operator_list[1] == '*'):
+        final_result = myMul((myDiv(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '/') and (operator_list[1] == '+'):
+        final_result = myAdd((myDiv(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '/') and (operator_list[1] == '-'):
+        final_result = mySub((myDiv(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '*') and (operator_list[1] == '*'):
+        final_result = myMul((myMul(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '*') and (operator_list[1] == '/'):
+        final_result = myDiv((myMul(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '*') and (operator_list[1] == '+'):
+        final_result = myAdd((myMul(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '*') and (operator_list[1] == '-'):
+        final_result = mySub((myMul(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '-') and (operator_list[1] == '-'):
+        final_result = mySub((mySub(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '-') and (operator_list[1] == '/'):
+        final_result = myDiv((mySub(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '-') and (operator_list[1] == '*'):
+        final_result = myMul((mySub(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '-') and (operator_list[1] == '+'):
+        final_result = myAdd((mySub(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '+') and (operator_list[1] == '+'):
+        final_result = myAdd((myAdd(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '+') and (operator_list[1] == '-'):
+        final_result = mySub((myAdd(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '+') and (operator_list[1] == '/'):
+        final_result = myDiv((myAdd(value_list[0], value_list[1])), value_list[2]) 
+    if (operator_list[0] == '+') and (operator_list[1] == '*'):
+        final_result = myMul((myAdd(value_list[0], value_list[1])), value_list[2]) 
 
 
-    final_result = 1
+
     return final_result
 
 
@@ -47,13 +82,13 @@ def display(operator_list, integers_list):
     final_result = evaluate_expression(operator_list, integers_list)
     operator_list.append('=')
     integers_list.append(final_result)
-    print("The result of the entered equation is:\n\t{} {} {} {} {} {} {}\n".format(integers_list[0], operator_list[0], integers_list[1], operator_list[1], integers_list[2], operator_list[2], integers_list[3]))
+    print("\nThe result of the entered equation is:\n\t{} {} {} {} {} {} {}\n".format(integers_list[0], operator_list[0], integers_list[1], operator_list[1], integers_list[2], operator_list[2], integers_list[3]))
 
     return
 
 def main():
     words = ['first', 'second', 'third']
-    first_integer = input('Please enter the first integer: ')
+    first_integer = input('\nPlease enter the first integer: ')
     if validate_integer(first_integer, words[0]) == 1:
         first_operator = str(input('Please enter the first operator: '))
         if validate_operator(first_operator, words[0]) == 1:
