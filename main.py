d18a29926d01ff7fdcd2bb342e479583eb7bac5c
IshaHaider <isha.haider213@gmail.com>
2022-10-09 16:38:40 -0600

    redefine validating functions

--- main.py
+++ main.py
@@ -24,17 +24,19 @@ def myDiv(value_one, value_two):
     result = operator_dict['/'](value_one, value_two)
     return result
 
-def validate_integer(value, str):
-    while value.isnumeric() != True:
+def validate_integer(str):
+    value = input(('Please enter the {} integer: ').format(str))
+    while value.lstrip("-").isdigit() != True:
         print("Error: incorrect integer input")
         value = input(('Please enter the {} integer: ').format(str))
-    return 1
+    return value
 
-def validate_operator(operator, str):
+def validate_operator(str):
+    operator = input(('Please enter the {} operator: ').format(str))
     while operator not in operator_dict.keys():
         print("Error: incorrect operator input")
         operator = input(('Please enter the {} operator: ').format(str))
-    return 1
+    return operator
 
 def evaluate_expression(operator_list, integers_list): 
     value_list = []
@@ -75,7 +77,6 @@ def evaluate_expression(operator_list, integers_list):
 
     return final_result
 
-
 def display(operator_list, integers_list):
     final_result = evaluate_expression(operator_list, integers_list)
     operator_list.append('=')
@@ -86,23 +87,21 @@ def display(operator_list, integers_list):
 
 def main():
     words = ['first', 'second', 'third']
-    first_integer = input('\nPlease enter the first integer: ')
-    if validate_integer(first_integer, words[0]) == 1:
-        first_operator = str(input('Please enter the first operator: '))
-        if validate_operator(first_operator, words[0]) == 1:
-            second_integer = input('Please enter the second integer: ')
-            if validate_integer(second_integer, words[1]) == 1:
-                second_operator = str(input('Please enter the second operator: '))
-                if validate_operator(second_operator, words[1]) == 1:
-                    third_integer = input('Please enter the third integer: ')
-                    if validate_integer(third_integer, words[2]) == 1:
-                        operator_list = [first_operator, second_operator] 
-                        integers_list = [first_integer, second_integer, third_integer]
-    
-    display(operator_list, integers_list)
-    return
+    print("\nWelcome to the terminal calculator!\nYou will be asked to enter a number of integers and operators to perform a basic calculation.\nPlease follow the instructions provided.")
+    integers_list = []
+    operator_list = []
+    integers_list.append(validate_integer(words[0]))
+    operator_list.append(validate_operator(words[0]))
+    integers_list.append(validate_integer(words[1]))
+    operator_list.append(validate_operator(words[1]))
+    integers_list.append(validate_integer(words[2]))
 
-    
+    print("Operator's List: {}". format(operator_list))
+    print("Integer's List: {}". format(integers_list))
+
+    display(operator_list, integers_list)
+    print("Thank you for using this terminal calculator.")
+    return
 
 if __name__ == "__main__":
     main() 
